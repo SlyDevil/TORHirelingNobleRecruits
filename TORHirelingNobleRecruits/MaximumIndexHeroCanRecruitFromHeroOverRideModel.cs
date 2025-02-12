d6c8d8a6dbab4ad36ba1d512c5083241a972d83f
SlyDevil <dkmac.15@gmail.com>
2025-02-12 16:49:57 -0500

    works now

--- TORHirelingNobleRecruits/MaximumIndexHeroCanRecruitFromHeroOverRideModel.cs
+++ TORHirelingNobleRecruits/MaximumIndexHeroCanRecruitFromHeroOverRideModel.cs
@@ -9,6 +9,8 @@ using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Settlements;
 using TOR_Core.Extensions;
 using TOR_Core.Models;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.Library;
 
 namespace TORHirelingNobleRecruits
 {
@@ -43,23 +45,82 @@ namespace TORHirelingNobleRecruits
         {
             if (this.GetBasicVolunteer(sellerHero).IsUndead())
             {
-                if (buyerHero.IsNecromancer())
+                if (!buyerHero.IsNecromancer() || !buyerHero.PartyBelongedTo.GetMemberHeroes().Any((Hero x) => x.IsNecromancer()))
                 {
-                    if (buyerHero.PartyBelongedTo.GetMemberHeroes().Any((Hero x) => x.IsNecromancer()))
-                    {
-                        goto HasNecro;
-                    }
+                    return -1;
                 }
-                return -1;
-            }
-
-            HasNecro:
-            int num = Campaign.Current.Models.VolunteerModel.MaximumIndexHeroCanRecruitFromHero(buyerHero, sellerHero, useValueAsRelation);
-            if (buyerHero == Hero.MainHero && buyerHero.IsEnlisted())
+            }            
+                
+            if (buyerHero.IsEnlisted() && buyerHero == Hero.MainHero)
             {
                 return -1;
             }
+
+            int num = baseMaximumIndexHeroCanRecruitFromHero(buyerHero, sellerHero, useValueAsRelation);
             return num;
         }
+
+        //this is dumb but idk how to get the Default model
+        //i think that TORVolunteer overriding the default model removes the definition for the default one? or at least in a way that blocks me from accessing it
+        public int baseMaximumIndexHeroCanRecruitFromHero(Hero buyerHero, Hero sellerHero, int useValueAsRelation = -101)
+		{
+			Settlement currentSettlement = sellerHero.CurrentSettlement;
+			int num = 1;
+			int num2 = ((buyerHero == Hero.MainHero) ? Campaign.Current.Models.DifficultyModel.GetPlayerRecruitSlotBonus() : 0);
+			int num3 = ((buyerHero != Hero.MainHero) ? 1 : 0);
+			int num4 = ((currentSettlement != null && buyerHero.MapFaction == currentSettlement.MapFaction) ? 1 : 0);
+			int num5 = ((currentSettlement != null && buyerHero.MapFaction.IsAtWarWith(currentSettlement.MapFaction)) ? (-(1 + num3)) : 0);
+			if (buyerHero.IsMinorFactionHero && currentSettlement != null && currentSettlement.IsVillage)
+			{
+				num5 = 0;
+			}
+			int num6 = ((useValueAsRelation < -100) ? buyerHero.GetRelation(sellerHero) : useValueAsRelation);
+			int num7 = ((num6 >= 100) ? 7 : ((num6 >= 80) ? 6 : ((num6 >= 60) ? 5 : ((num6 >= 40) ? 4 : ((num6 >= 20) ? 3 : ((num6 >= 10) ? 2 : ((num6 >= 5) ? 1 : ((num6 >= 0) ? 0 : (-1)))))))));
+			int num8 = 0;
+			if (sellerHero.IsGangLeader && currentSettlement != null && currentSettlement.OwnerClan == buyerHero.Clan)
+			{
+				if (currentSettlement.IsTown)
+				{
+					Hero governor = currentSettlement.Town.Governor;
+					if (governor != null && governor.GetPerkValue(DefaultPerks.Roguery.OneOfTheFamily))
+					{
+						goto IL_014E;
+					}
+				}
+				if (!currentSettlement.IsVillage)
+				{
+					goto IL_015E;
+				}
+				Hero governor2 = currentSettlement.Village.Bound.Town.Governor;
+				if (governor2 == null || !governor2.GetPerkValue(DefaultPerks.Roguery.OneOfTheFamily))
+				{
+					goto IL_015E;
+				}
+				IL_014E:
+				num8 += (int)DefaultPerks.Roguery.OneOfTheFamily.SecondaryBonus;
+			}
+			IL_015E:
+			if (sellerHero.IsMerchant && buyerHero.GetPerkValue(DefaultPerks.Trade.ArtisanCommunity))
+			{
+				num8 += (int)DefaultPerks.Trade.ArtisanCommunity.SecondaryBonus;
+			}
+			if (sellerHero.Culture == buyerHero.Culture && buyerHero.GetPerkValue(DefaultPerks.Leadership.CombatTips))
+			{
+				num8 += (int)DefaultPerks.Leadership.CombatTips.SecondaryBonus;
+			}
+			if (sellerHero.IsRuralNotable && buyerHero.GetPerkValue(DefaultPerks.Charm.Firebrand))
+			{
+				num8 += (int)DefaultPerks.Charm.Firebrand.SecondaryBonus;
+			}
+			if (sellerHero.IsUrbanNotable && buyerHero.GetPerkValue(DefaultPerks.Charm.FlexibleEthics))
+			{
+				num8 += (int)DefaultPerks.Charm.FlexibleEthics.SecondaryBonus;
+			}
+			if (sellerHero.IsArtisan && buyerHero.PartyBelongedTo != null && buyerHero.PartyBelongedTo.EffectiveEngineer != null && buyerHero.PartyBelongedTo.EffectiveEngineer.GetPerkValue(DefaultPerks.Engineering.EngineeringGuilds))
+			{
+				num8 += (int)DefaultPerks.Engineering.EngineeringGuilds.PrimaryBonus;
+			}
+			return MathF.Min(6, MathF.Max(0, num + num3 + num7 + num2 + num4 + num5 + num8));
+		}
     }
 }
--- TORHirelingNobleRecruits/SubModule.cs
+++ TORHirelingNobleRecruits/SubModule.cs
@@ -1,5 +1,10 @@
 ﻿using TaleWorlds.MountAndBlade;
 using TaleWorlds.Library;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using System.Linq;
+using TaleWorlds.CampaignSystem.ComponentInterfaces;
+using TaleWorlds.Localization;
 
 
 namespace TORHirelingNobleRecruits
@@ -15,6 +20,32 @@ namespace TORHirelingNobleRecruits
         {
             base.OnSubModuleUnloaded();
 
+        }
+        
+        public override void OnGameInitializationFinished(Game game)
+        {
+            base.OnGameInitializationFinished(game);
+        }
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (Game.Current.GameType is Campaign && gameStarterObject is CampaignGameStarter)
+            {
+                var existingModel = GetGameModel<VolunteerModel>(gameStarterObject);
+                //GetGameModel will return the default model if it doesn't find any others?
+                gameStarterObject.AddModel(new MaximumIndexHeroCanRecruitFromHeroOverRideModel(existingModel));
+            }
+        }
+
+        private T? GetGameModel<T>(IGameStarter gameStarterObject) where T : GameModel
+        {
+            var models = gameStarterObject.Models.ToArray();
+
+            for (int index = models.Length - 1; index >= 0; --index)
+            {
+                if (models[index] is T gameModel1)
+                    return gameModel1;
+            }
+            return default;
         }
 
         protected override void OnBeforeInitialModuleScreenSetAsRoot()
